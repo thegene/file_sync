c0a7d4189a40396cbdd9ed894f399764b977a9b5
Eugene Westbrook <eugenewestbrook@gmail.com>
2017-12-22 13:11:17 -0600

    Make the download script work again

--- scripts/download.exs
+++ scripts/download.exs
@@ -4,8 +4,7 @@ alias FileSync.Boundaries.DropBox.HttpApi
 alias FileSync.Boundaries.DropBox.Endpoints.Download
 
 opts = %{
-  #endpoint: %Download{path: "/harrison birth/img_0305.jpg"}
-  endpoint: %Download{path: "/harrison birth/img_.jpg"}
+  endpoint: %Download{path: "/harrison birth/img_0305.jpg"}
 }
 
 response = HttpApi.post(opts)
