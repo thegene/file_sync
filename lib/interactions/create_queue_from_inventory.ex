4fbad51d30071f6c6bddb7b9f4019aadc6b0f4e0
Eugene Westbrook <eugenewestbrook@gmail.com>
2018-02-17 16:50:00 -0600

    Introduces a CreateQueueFromInventory Interaction

---
+++ lib/interactions/create_queue_from_inventory.ex
@@ -0,0 +1,19 @@
+defmodule FileSync.Interactions.CreateQueueFromInventory do
+
+  alias FileSync.Interactions.Queue
+  alias FileSync.Data.InventoryItem
+
+  def push_to_queue(inventory_response, {:ok, queue}) do
+    {:ok, list} = inventory_response
+
+    list
+    |> Enum.each(&add_to_queue(&1, queue))
+  end
+
+  def add_to_queue(item = %InventoryItem{}, queue) do
+    queue |> Queue.push(item)
+  end
+
+  def add_to_queue(_item, _queue) do
+  end
+end
---
+++ spec/interactions/create_queue_from_inventory.exs
@@ -0,0 +1,60 @@
+defmodule FileSync.Interactions.CreateQueueFromInventorySpec do
+  use ESpec
+
+  alias FileSync.Interactions.{Queue,CreateQueueFromInventory}
+  alias FileSync.Data.{InventoryFolder,InventoryItem}
+
+  context "Given a queue and a successful inventory response" do
+    let queue: Queue.start_link([])
+    let :inventory_response do
+      {:ok, [
+        %InventoryItem{name: "some item"},
+        %InventoryFolder{}
+      ]}
+    end
+
+    it "starts empty for sanity" do
+      queue()
+      |> elem(1)
+      |> Queue.empty?
+      |> expect
+      |> to(be_true())
+    end
+
+    context "when we add to the queue" do
+      before do
+        inventory_response() |> CreateQueueFromInventory.push_to_queue(queue())
+      end
+
+      let :inventory_item do
+        queue()
+        |> elem(1)
+        |> Queue.pop
+      end
+
+      it "is no longer empty" do
+        queue()
+        |> elem(1)
+        |> Queue.empty?
+        |> expect
+        |> not_to(be_true())
+      end
+
+      it "contains the inventory item" do
+        inventory_item()
+        |> Map.get(:name)
+        |> expect
+        |> to(eq("some item"))
+      end
+
+      it "does not contain the folder" do
+        inventory_item()
+        queue()
+        |> elem(1)
+        |> Queue.empty?
+        |> expect
+        |> to(be_true())
+      end
+    end
+  end
+end
