a3fdd6adbbd8261db704701062a2ceda2f7fc375
Eugene Westbrook <eugenewestbrook@gmail.com>
2018-04-07 14:04:10 -0500

    Finally chunking the content for content_hash

--- lib/boundaries/drop_box/content_hash_validator.ex
+++ lib/boundaries/drop_box/content_hash_validator.ex
@@ -8,7 +8,24 @@ defmodule FileSync.Boundaries.DropBox.ContentHashValidator do
                        content: content,
                        name: name,
                        source_meta: %SourceMeta{content_hash: hash}}) do
+    chunks = content
+    |> get_chunks
+  end
+
+  defp get_chunks(content) do
+    chunks = get_next_chunk(content)
     IEx.pry
   end
 
+  defp get_next_chunk(content, start \\ 0, memo \\ [])
+  defp get_next_chunk(content, start, memo) when byte_size(content) <= start + 4194304 do
+    len = byte_size(content) - start
+    memo ++ [content |> binary_part(start, len)]
+  end
+
+  defp get_next_chunk(content, start, memo) do
+    new_memo = memo ++ [content |> binary_part(start, 4194304)]
+
+    content |> get_next_chunk(start + 4194304, new_memo)
+  end
 end
