db82fa7d7b4bba4b73c379304e399ce47c4668d8
Eugene Westbrook <eugenewestbrook@gmail.com>
2018-04-12 11:12:31 -0500

    Forgot to clean up this queue sync script

--- scripts/sync_queues.exs
+++ scripts/sync_queues.exs
@@ -1,32 +1,17 @@
 require IEx
 
-alias FileSync.Interactions
-alias Interactions.Queue
+alias FileSync.Actions
+alias Actions.Queue
 alias FileSync.Boundaries.DropBox
 
-from = %Interactions.Source{
-  module: %FileSync.Boundaries.DropBox{},
-  opts: %{
-    folder: "Harrison Birth",
-    limit: 30
-  }
-}
-
-to = %Interactions.Source{
-  module: %FileSync.Boundaries.FileSystem{},
-  opts: %{
-    directory: "data"
-  }
-}
-
-{:ok, inventory_queue} = Interactions.Queue.start_link([])
-{:ok, item_queue} = Interactions.Queue.start_link([])
+{:ok, inventory_queue} = Queue.start_link([])
+{:ok, item_queue} = Queue.start_link([])
 
 DropBox.Inventory.get(%{folder: "Harrison Birth", limit: 3})
-|> Interactions.InventoryQueue.push_to_queue(inventory_queue)
+|> Actions.BuildInventoryQueue.push_to_queue(inventory_queue)
 
 inventory_queue
-|> Interactions.BuildFileDataQueue.process_to(
+|> Actions.BuildFileDataQueue.process_to(
   item_queue,
   DropBox.FileContents,
   [DropBox.ContentHashValidator]
