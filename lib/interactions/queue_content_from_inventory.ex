143e278617d3bc5e90e408cfec2d26fd6cfa5f1a
Eugene Westbrook <eugenewestbrook@gmail.com>
2018-04-12 13:52:50 -0500

    Handle failed file content data

--- lib/interactions/queue_content_from_inventory.ex
+++ lib/interactions/queue_content_from_inventory.ex
@@ -4,20 +4,32 @@ defmodule FileSync.Interactions.QueueContentFromInventoryQueue do
   alias FileSync.Data.InventoryItem
 
   def process(inventory_queue, content_queue, source, opts \\ %{}) do
-    inventory_queue
-    |> Queue.pop
+    item = inventory_queue |> Queue.pop
+
+    item
     |> get_data(source, opts)
-    |> enqueue(inventory_queue, content_queue, opts)
+    |> enqueue(inventory_queue, content_queue, item, opts)
   end
 
   defp get_data(item = %InventoryItem{}, source, opts) do
     item |> source.contents.get(opts)
   end
 
-  defp enqueue({:ok, file_data}, _inventory_queue, content_queue, opts) do
+  defp enqueue({:ok, file_data}, _inventory_queue, content_queue, _original_item, opts) do
     content_queue |> Queue.push(file_data)
 
     opts |> info("successfully enqueued content for #{file_data.name}")
+  end
+
+  defp enqueue({:error, message}, inventory_queue, _content_queue, original_item, opts) do
+    inventory_queue |> Queue.push(original_item)
+
+    opts |> error("failed getting #{original_item.name}, will retry: #{message}")
+  end
+
+  defp error(opts, message) do
+    logger = logger_from_opts(opts)
+    logger.error(message)
   end
 
   defp info(opts, message) do
--- spec/interactions/queue_content_from_inventory_queue_spec.exs
+++ spec/interactions/queue_content_from_inventory_queue_spec.exs
@@ -23,14 +23,14 @@ defmodule FileSync.Interactions.QueueContentFromInventoryQueueSpec do
     end
 
     context "when an item is on the inventory queue" do
-      let item: %InventoryItem{path: "somewhere"}
-      let validators: [validator()]
+      let item: %InventoryItem{path: "somewhere", name: "FooFile.png"}
       let source: %Source{contents: file_contents(), validators: validators()}
 
       let :mock_logger do
         Logger
         |> double
         |> allow(:info, fn(_msg) -> nil end)
+        |> allow(:error, fn(_msg) -> nil end)
       end
 
       let :opts do
@@ -48,6 +48,8 @@ defmodule FileSync.Interactions.QueueContentFromInventoryQueueSpec do
       end
 
       context "when we successfully get the contents" do
+        let validators: [validator()]
+
         let :file_contents do
           DropBox.FileContents
           |> double
@@ -74,6 +76,35 @@ defmodule FileSync.Interactions.QueueContentFromInventoryQueueSpec do
           it "logs a successful enqueue" do
             assert_received({:info, "successfully enqueued content for FooFile.png"})
           end
+        end
+      end
+
+      context "when getting contents fails" do
+        let validators: []
+
+        let :file_contents do
+          DropBox.FileContents
+          |> double
+          |> allow(:get, fn(_data, _opts) -> {:error, "something borked"} end)
+        end
+
+        it "adds the inventory item back to the inventory queue" do
+          inventory_queue()
+          |> Queue.pop
+          |> expect
+          |> to(eq(item()))
+        end
+
+        it "leaves the content queue empty" do
+          content_queue()
+          |> Queue.empty?
+          |> expect
+          |> to(eq(true))
+        end
+
+        it "logs the error message" do
+          assert_received({:error,
+            "failed getting FooFile.png, will retry: something borked"})
         end
       end
     end
