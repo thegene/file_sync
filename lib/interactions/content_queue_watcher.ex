b69b2ea600766ac8796e5ba04e5539fc40b82399
Eugene Westbrook <eugenewestbrook@gmail.com>
2018-07-19 21:05:43 -0500

    Copy over stuff from the other watcher module

--- lib/interactions/content_queue_watcher.ex
+++ lib/interactions/content_queue_watcher.ex
@@ -1,4 +1,5 @@
 defmodule FileSync.Interactions.ContentQueueWatcher do
+  use GenServer
 
   alias FileSync.Interactions.{
     SaveContentQueueToInventory,
@@ -13,5 +14,26 @@ defmodule FileSync.Interactions.ContentQueueWatcher do
 
   def handle_response({_success, message}, logger) do
     logger.warn(message)
+  end
+
+  def start_link(content_queue, target) do
+    state = %{content_queue: content_queue, target: target}
+    GenServer.start_link(__MODULE__, state)
+  end
+
+  def init(state) do
+    schedule()
+    {:ok, state}
+  end
+
+  defp schedule, do: Process.send_after(self(), :watch, delay())
+
+  defp delay, do: 1000 * 10
+
+  def handle_info(:watch, state) do
+    check_queue(
+      state.content_queue,
+      state.target
+    )
   end
 end
