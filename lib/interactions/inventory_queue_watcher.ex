9d6a202ef57493a89c37ae60805f45e2c266cc5b
Eugene Westbrook <eugenewestbrook@gmail.com>
2018-07-19 20:23:00 -0500

    Laying inventory queue watcher out as a genserver task

--- lib/interactions/inventory_queue_watcher.ex
+++ lib/interactions/inventory_queue_watcher.ex
@@ -2,9 +2,38 @@ defmodule FileSync.Interactions.InventoryQueueWatcher do
   use GenServer
 
   alias FileSync.Interactions.QueueContentFromInventoryQueue
+  alias FileSync.Data.FileData
 
-  def init(inventory_queue, content_queue, source) do
+  def start_link(inventory_queue, content_queue, source) do
+    state = build_state(inventory_queue, content_queue, source)
+    GenServer.start_link(__MODULE__, state)
+  end
 
+  def init(state) do
+    schedule()
+    {:ok, state}
+  end
+
+  defp build_state(inventory_queue, content_queue, source) do
+    %{
+      inventory_queue: inventory_queue,
+      content_queue: content_queue,
+      source: source
+    }
+  end
+
+  defp schedule, do: Process.send_after(self(), :watch, delay())
+
+  defp delay, do: 1000 * 10
+
+  def handle_info(:watch, state) do
+    check_queue(
+      state.inventory_queue,
+      state.content_queue,
+      state.source
+    )
+    schedule()
+    {:noreply, state}
   end
 
   def check_queue(inventory_queue, content_queue, source, module \\ QueueContentFromInventoryQueue) do
@@ -16,7 +45,12 @@ defmodule FileSync.Interactions.InventoryQueueWatcher do
     |> handle_response(source.logger)
   end
 
-  defp handle_response({:ok, _message}, _logger) do
+  defp handle_response({:ok, file_data = %FileData{}}, logger) do
+    logger.warn(file_data.name)
+  end
+
+  defp handle_response({:ok, message}, logger) do
+    logger.warn(message)
   end
 
   defp handle_response({:error, message}, logger) do
