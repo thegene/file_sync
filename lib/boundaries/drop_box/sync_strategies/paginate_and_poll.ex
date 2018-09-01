6051d761ad57a1280fb02671d20a192808dcded9
Eugene Westbrook <eugenewestbrook@gmail.com>
2018-09-01 11:19:51 -0500

    Introduces sync strategy for dropbox

---
+++ lib/boundaries/drop_box/sync_strategies/paginate_and_poll.ex
@@ -0,0 +1,38 @@
+defmodule FileSync.Boundaries.DropBox.SyncStrategies.PaginateAndPoll do
+
+  alias FileSync.Interactions.Source
+  alias FileSync.Actions.Queue
+  alias FileSync.Boundaries.DropBox.{
+    Client,
+    Response
+  }
+  
+  def check(opts) do
+    with_defaults = opts |> Enum.into(%{client: Client})
+
+    list_folder(with_defaults)
+    |> handle_response(with_defaults)
+  end
+
+  defp list_folder(%{client: client, source: %Source{opts: source_opts}}) do
+    %{
+      folder: source_opts[:folder],
+      limit: source_opts[:limit] || 100
+    }
+    |> client.list_folder
+  end
+
+  defp handle_response({:ok, %Response{body: body}}, %{queue: queue}) do
+    body[:entries]
+    |> Enum.each(fn(item) -> queue |> Queue.push(item) end)
+
+    {:ok, %{
+      has_more: body[:has_more],
+      cursor: body[:cursor]
+    }}
+  end
+
+  defp handle_response({:error, message}, _defaults) do
+    {:error, message}
+  end
+end
---
+++ spec/boundaries/drop_box/sync_strategies/paginate_and_poll_spec.exs
@@ -0,0 +1,108 @@
+defmodule FileSync.Boundaries.DropBox.SyncStrategies.PaginateAndPollSpec do
+  use ESpec
+
+  import Double
+
+  alias FileSync.Interactions.Source
+  alias FileSync.Actions.Queue
+  alias FileSync.Boundaries.DropBox.SyncStrategies.PaginateAndPoll
+  alias FileSync.Boundaries.DropBox.{
+    Client,
+    Response
+  }
+
+  context "Given we are checking for DropBox inventory" do
+    let :check do
+      PaginateAndPoll.check(
+        source: source(),
+        last_response: last_response(),
+        client: mock_client(),
+        queue: queue()
+      )
+    end
+
+    let queue: Queue.start_link |> elem(1)
+
+    let :source do
+      %Source{
+        opts: %{
+          folder: "foo",
+          limit: 5
+        }
+      }
+    end
+
+    context "with no data" do
+      let last_response: %{}
+
+      context "and the request is successful" do
+        let :mock_client do
+          Client
+          |> double
+          |> allow(:list_folder, fn(%{folder: "foo", limit: 5}) ->
+            {:ok,  %Response{
+                body: %{
+                  entries: ["foo", "bar"],
+                  cursor: "ABC",
+                  has_more: false
+                }
+              }
+            }
+          end)
+        end
+
+        it "requests a list of a folder from the client" do
+          check()
+          assert_received({:list_folder, %{folder: "foo", limit: 5}})
+        end
+
+        it "returns the cursor and has_more boolean" do
+          check()
+          |> expect
+          |> to(eq({:ok, %{
+            cursor: "ABC",
+            has_more: false
+          }}))
+        end
+
+        it "enqueues the entries" do
+          check()
+
+          queue()
+          |> Queue.pop
+          |> expect
+          |> to(eq("foo"))
+
+          queue()
+          |> Queue.pop
+          |> expect
+          |> to(eq("bar"))
+        end
+      end
+
+      context "and the request returns an error" do
+        let :mock_client do
+          Client
+          |> double
+          |> allow(:list_folder, fn(%{folder: "foo", limit: 5}) ->
+            {:error, "Path not found"}
+          end)
+        end
+
+        it "does not enqueue anything" do
+          check()
+          queue()
+          |> Queue.empty?
+          |> expect
+          |> to(eq(true))
+        end
+
+        it "returns the error" do
+          check()
+          |> expect
+          |> to(eq({:error, "Path not found"}))
+        end
+      end
+    end
+  end
+end
