9aadfeb0ff5b6be74f4f6644a19005ab4af47009
Eugene Westbrook <eugenewestbrook@gmail.com>
2018-12-30 12:32:32 -0600

    Sync watcher now does periodic checks. need to add in a script to try it out

--- lib/interactions/sync_strategy_watcher.ex
+++ lib/interactions/sync_strategy_watcher.ex
@@ -1,6 +1,39 @@
 defmodule FileSync.Interactions.SyncStrategyWatcher do
+  use GenServer
+
   alias FileSync.Interactions.Source
+  alias FileSync.Actions.Queue
   
+  def start_link(inventory_queue: queue_name, source: source) do
+    state = %{
+      queue: Queue: find_queue(queue_name),
+      source: source,
+      last_response: %{}
+    }
+    GenServer.start_link(__MODULE__, state)
+  end
+
+  @impl true
+  def init(state) do
+    schedule()
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:poll, state) do
+    last_response = poll(
+      state.last_response,
+      state.source,
+      state.queue
+    )
+
+    {:noreply, state |> Map.merge({last_response: last_response})}
+  end
+
+  defp schedule, do: Process.send_after(self(), :poll, delay())
+
+  defp delay, do: 1000 * 60 * 5
+
   def poll({:ok, last_response}, source = %Source{}, queue) do
     source.strategy.check(
       last_response,
