4ed1ff82626e9fcef84797e62b6027b0ee956440
Eugene Westbrook <eugenewestbrook@gmail.com>
2018-12-30 12:14:04 -0600

    Strategy watcher now logs errors

--- lib/interactions/sync_strategy_watcher.ex
+++ lib/interactions/sync_strategy_watcher.ex
@@ -6,7 +6,7 @@ defmodule FileSync.Interactions.SyncStrategyWatcher do
       last_response,
       source,
       queue
-    )
+    ) |> handle_response(source)
   end
 
   def poll(_last_response, source = %Source{}, strategy, queue) do
@@ -14,6 +14,15 @@ defmodule FileSync.Interactions.SyncStrategyWatcher do
       %{},
       source,
       queue
-    )
+    ) |> handle_response(source)
+  end
+
+  defp handle_response(last_response = {:ok, _anything}, _source) do
+    last_response
+  end
+
+  defp handle_response(last_response = {:error, message}, %Source{logger: logger}) do
+    logger.warn(message)
+    last_response
   end
 end
--- spec/interactions/sync_strategy_watcher_spec.exs
+++ spec/interactions/sync_strategy_watcher_spec.exs
@@ -11,49 +11,80 @@ defmodule FileSync.Interactions.SyncStrategyWatcherSpec do
   alias FileSync.Boundaries.DropBox.SyncStrategies.Paginate
 
   context "Given a strategy and an inventory queue" do
-    let :mock_strategy do
-      Paginate
-      |> double
-      |> allow(:check, fn(
-          _last_response,
-          %Source{},
-          _queue
-        ) -> {:ok, "foobar"} end)
-      |> allow(:check, fn(
-          "foobar",
-          %Source{},
-          _queue
-        ) -> {:ok, "blah"} end)
-    end
 
     let queue: Queue.start_link |> elem(1)
 
-    let source: %Source{}
+    let source: %Source{logger: mock_logger()}
 
-    it "checks the source with the given strategy" do
-      SyncStrategyWatcher.poll(%{}, source(), mock_strategy(), queue())
-
-      assert_received({
-        :check,
-        %{},
-        %Source{},
-        _queue_pid
-      })
+    let :mock_logger do
+      Logger
+      |> double
+      |> allow(:warn, fn(_msg) -> nil end)
+      |> allow(:info, fn(_msg) -> nil end)
     end
 
-    context "when passed the response from the first call" do
-      let :last_response do
-        SyncStrategyWatcher.poll(%{}, source(), mock_strategy(), queue())
+    context "when checks are successful" do
+      let :mock_strategy do
+        Paginate
+        |> double
+        |> allow(:check, fn(
+            _last_response,
+            %Source{},
+            _queue
+          ) -> {:ok, "foobar"} end)
+        |> allow(:check, fn(
+            "foobar",
+            %Source{},
+            _queue
+          ) -> {:ok, "blah"} end)
       end
 
-      it "calls the strategy with the first response" do
-        SyncStrategyWatcher.poll(last_response(), source(), mock_strategy(), queue())
+      it "checks the source with the given strategy" do
+        SyncStrategyWatcher.poll(%{}, source(), mock_strategy(), queue())
 
         assert_received({
           :check,
-          "foobar",
+          %{},
           %Source{},
           _queue_pid
+        })
+      end
+
+      context "when passed the response from the first call" do
+        let :last_response do
+          SyncStrategyWatcher.poll(%{}, source(), mock_strategy(), queue())
+        end
+
+        it "calls the strategy with the first response" do
+          SyncStrategyWatcher.poll(last_response(), source(), mock_strategy(), queue())
+
+          assert_received({
+            :check,
+            "foobar",
+            %Source{},
+            _queue_pid
+          })
+        end
+      end
+    end
+
+    context "when there is an error" do
+      let :mock_strategy do
+        Paginate
+        |> double
+        |> allow(:check, fn(
+            _last_response,
+            %Source{},
+            _queue
+          ) -> {:error, "borked"} end)
+      end
+
+      it "logs that an error occurred" do
+        SyncStrategyWatcher.poll(%{}, source(), mock_strategy(), queue())
+
+        assert_received({
+          :warn,
+          "borked"
         })
       end
     end
